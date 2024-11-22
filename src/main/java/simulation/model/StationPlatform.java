5fa70b65732ded7690b671ef02c717d59142ab65
Minimal-Alexi <alexpop@metropolia.fi>
2024-11-22 07:58:04 +0200

    Created the StationPlatform class. Created a trainDeparture function.

--- src/main/java/simulation/model/StationPlatform.java
+++ src/main/java/simulation/model/StationPlatform.java
@@ -1,7 +1,26 @@
-/*
 package simulation.model;
 
+import java.util.Random;
+import simulation.framework.Clock;
+
+import java.util.ArrayList;
 public class StationPlatform extends ServicePoint{
-    public StationPlatform(int id, String name, int x, int y, int z) {}
+
+    private int trainCapacity,minimumTrainCapacity,maximumTrainCapacity;
+
+    public StationPlatform(String serviceName, int minimumResponseTime, int maximumResponseTime,int minimumTrainCapacity, int maximumTrainCapacity) {
+        super(serviceName,new ArrayList<ServicePoint>(),minimumResponseTime,maximumResponseTime);
+        this.minimumTrainCapacity = minimumTrainCapacity;
+        this.maximumTrainCapacity = maximumTrainCapacity;
+    }
+    public void trainDeparture(Clock clock){
+        Random rand = new Random();
+        this.trainCapacity = rand.nextInt(this.minimumTrainCapacity,this.maximumTrainCapacity);
+        int currentTrainCapacity = this.trainCapacity;
+        while(!super.isQueueEmpty() && currentTrainCapacity > 0){
+            super.servePassenger(clock);
+            currentTrainCapacity--;
+        }
+    }
 }
-*/
+
