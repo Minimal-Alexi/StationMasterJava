218a8e0581030eb40a9fd2ba37bf17dcb31f3505
Minimal-Alexi <alexpop@metropolia.fi>
2024-11-27 12:34:39 +0200

    Reworked all of the project. We will follow the guide.

--- src/main/java/simulation/model/Passenger.java
+++ src/main/java/simulation/model/Passenger.java
@@ -1,13 +1,14 @@
 package simulation.model;
 
 public class Passenger {
+    private static int nr = 0;
     private int id;
     private int arrivalTime;
     private int departureTime;
     private int serviceEntryTime;
     private int serviceExitTime;
-    public Passenger(int id, int arrivalTime) {
-        this.id = id;
+    public Passenger(int arrivalTime) {
+        this.id = nr++;
         this.arrivalTime = arrivalTime;
     }
     public void setArrivalTime(int arrivalTime) {
