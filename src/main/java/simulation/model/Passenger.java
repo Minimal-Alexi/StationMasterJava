b8ab0c9329ca0f2b216edabcc8f16262134b8d85
Minimal-Alexi <alexpop@metropolia.fi>
2024-11-29 10:42:48 +0200

    Created SeedSingleton for future use. Passenger class tested.

--- src/main/java/simulation/model/Passenger.java
+++ src/main/java/simulation/model/Passenger.java
@@ -1,17 +1,28 @@
 package simulation.model;
 
+import eduni.distributions.Bernoulli;
+import simulation.framework.Clock;
+import simulation.framework.SeedSingleton;
+
 public class Passenger {
     private static int nr = 0;
+    private static final double TRAIN_METRO_RATIO = 0.3;
+    private static Bernoulli bernoulli = new Bernoulli(TRAIN_METRO_RATIO);
     private final int id;
     private final PassengerType type;
     private long arrivalTime;
     private long departureTime;
     private long serviceEntryTime;
     private long serviceExitTime;
-    public Passenger(long arrivalTime, PassengerType type) {
+    public Passenger() {
         this.id = nr++;
-        this.arrivalTime = arrivalTime;
-        this.type = type;
+        this.arrivalTime = Clock.getInstance().getTime();
+        if(bernoulli.sample() == 1){
+            this.type = PassengerType.Train;
+        }
+        else{
+            this.type = PassengerType.Metro;
+        }
     }
     public void setArrivalTime(long arrivalTime) {
         this.arrivalTime = arrivalTime;
@@ -37,5 +48,18 @@ public class Passenger {
     public long getServiceTime(){
         return this.serviceExitTime - this.serviceEntryTime;
     }
-
+    public static void main(String[] args){
+        int metro = 0,train = 0;
+        for(int i=0; i<100;++i){
+            Passenger passenger = new Passenger();
+            if(passenger.getType() == PassengerType.Train){
+                train++;
+            }
+            else if(passenger.getType() == PassengerType.Metro){
+                metro++;
+            }
+        }
+        System.out.println(train);
+        System.out.println(metro);
+    }
 }
