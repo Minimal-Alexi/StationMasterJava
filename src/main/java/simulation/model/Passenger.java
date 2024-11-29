acc6ef9d518fbac0dc4461dedf4dae51ffb412e3
Minimal-Alexi <alexpop@metropolia.fi>
2024-11-29 09:07:41 +0200

    Added passenger type.

--- src/main/java/simulation/model/Passenger.java
+++ src/main/java/simulation/model/Passenger.java
@@ -3,13 +3,15 @@ package simulation.model;
 public class Passenger {
     private static int nr = 0;
     private final int id;
+    private final PassengerType type;
     private long arrivalTime;
     private long departureTime;
     private long serviceEntryTime;
     private long serviceExitTime;
-    public Passenger(long arrivalTime) {
+    public Passenger(long arrivalTime, PassengerType type) {
         this.id = nr++;
         this.arrivalTime = arrivalTime;
+        this.type = type;
     }
     public void setArrivalTime(long arrivalTime) {
         this.arrivalTime = arrivalTime;
@@ -25,6 +27,9 @@ public class Passenger {
     }
     public int getId(){
         return this.id;
+    }
+    public PassengerType getType(){
+        return this.type;
     }
     public long getStationTime(){
         return this.departureTime-this.arrivalTime;
