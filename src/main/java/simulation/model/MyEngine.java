0dda4c13e34e83682ba7385edd9d7380c8259e4b
Minimal-Alexi <alexpop@metropolia.fi>
2024-12-02 15:08:35 +0200

    Added more information printing for the engine. Refactored the TrainStations and ServicePoints to be in different lists.

--- src/main/java/simulation/model/MyEngine.java
+++ src/main/java/simulation/model/MyEngine.java
@@ -12,17 +12,19 @@ import simulation.framework.Event;
 import java.util.ArrayList;
 
 public class MyEngine extends Engine {
-    private ArrivalProcess[] arrivalProcesses;
-    private ServicePoint[] servicePoints;
+    private final TrainStation[] trainStations;
+    private final ArrivalProcess[] arrivalProcesses;
+    private final ServicePoint[] servicePoints;
 
     public MyEngine() {
         super();
-        servicePoints = new ServicePoint[4];
+        servicePoints = new ServicePoint[1];
+        trainStations = new TrainStation[3];
         arrivalProcesses = new ArrivalProcess[4];
-        servicePoints[0] = new ServicePoint("Ticket Check", new Normal(25, 20), eventList, EventType.B2_TICKET_CHECK_FINISH);
-        servicePoints[1] = new TrainStation("Train Station 1", new Normal(60, 120),new Normal(20,30), eventList, EventType.B3_TRAIN1_DEPARTURE);
-        servicePoints[2] = new TrainStation("Train Station 2", new Normal(60, 120),new Normal(20,30), eventList, EventType.B4_TRAIN2_DEPARTURE);
-        servicePoints[3] = new TrainStation("Metro Station", new Normal(30, 45),new Normal(50,10), eventList, EventType.B5_TRAIN3_DEPARTURE);
+        servicePoints[0] = new ServicePoint("Ticket Check", new Normal(20, 20), eventList, EventType.B2_TICKET_CHECK_FINISH);
+        trainStations[0] = new TrainStation("Train Station 1", new Normal(60, 120),new Normal(20,30), eventList, EventType.B3_TRAIN1_DEPARTURE);
+        trainStations[1] = new TrainStation("Train Station 2", new Normal(60, 120),new Normal(20,30), eventList, EventType.B4_TRAIN2_DEPARTURE);
+        trainStations[2] = new TrainStation("Metro Station", new Normal(30, 45),new Normal(50,10), eventList, EventType.B5_TRAIN3_DEPARTURE);
         arrivalProcesses[0] = new ArrivalProcess(new Negexp(20), eventList, EventType.B1_PASSENGER_ARRIVAL);
         arrivalProcesses[1] = new ArrivalProcess(new Normal(240, 600), eventList, EventType.B6_TRAIN1_ARRIVAL);
         arrivalProcesses[2] = new ArrivalProcess(new Normal(420, 60), eventList, EventType.B7_TRAIN2_ARRIVAL);
@@ -49,44 +51,44 @@ public class MyEngine extends Engine {
             case B2_TICKET_CHECK_FINISH:
                 passenger = servicePoints[0].removeFromQueue();
                 if (passenger.getType() == PassengerType.Train) {
-                    if (servicePoints[1].getQueueSize() > servicePoints[2].getQueueSize()) {
-                        servicePoints[2].addToQueue(passenger);
+                    if (trainStations[0].getQueueSize() > trainStations[1].getQueueSize()) {
+                        trainStations[1].addToQueue(passenger);
                     } else {
-                        servicePoints[1].addToQueue(passenger);
+                        trainStations[0].addToQueue(passenger);
                     }
                 } else {
-                    servicePoints[3].addToQueue(passenger);
+                    trainStations[2].addToQueue(passenger);
                 }
                 break;
             case B3_TRAIN1_DEPARTURE:
                 arrivalProcesses[1].generateNextEvent();
-                trainStation = (TrainStation) servicePoints[1];
+                trainStation = trainStations[0];
                 passengers = trainStation.loadTrain();
                 passengerListReport(passengers);
                 break;
             case B4_TRAIN2_DEPARTURE:
                 arrivalProcesses[2].generateNextEvent();
-                trainStation = (TrainStation) servicePoints[2];
+                trainStation = trainStations[1];
                 passengers = trainStation.loadTrain();
                 passengerListReport(passengers);
                 break;
             case B5_TRAIN3_DEPARTURE:
                 arrivalProcesses[3].generateNextEvent();
-                trainStation = (TrainStation) servicePoints[3];
+                trainStation = trainStations[2];
                 passengers = trainStation.loadTrain();
                 System.out.println(passengers);
                 passengerListReport(passengers);
                 break;
             case B6_TRAIN1_ARRIVAL:
-                trainStation = (TrainStation) servicePoints[1];
+                trainStation = trainStations[0];
                 trainStation.beginService();
                 break;
             case B7_TRAIN2_ARRIVAL:
-                trainStation = (TrainStation) servicePoints[2];
+                trainStation = trainStations[1];
                 trainStation.beginService();
                 break;
             case B8_TRAIN3_ARRIVAL:
-                trainStation = (TrainStation) servicePoints[3];
+                trainStation = trainStations[2];
                 trainStation.beginService();
                 break;
         }
@@ -105,10 +107,14 @@ public class MyEngine extends Engine {
         for (ServicePoint sp : servicePoints) {
             System.out.printf("Total passengers at %s: %d\nTotal passengers serviced: %d\nAverage service time: %.2f\n\n", sp.getName(), sp.getQueueSize(), sp.getCustomerServiced(), sp.getMeanServiceTime());
         }
+        for (TrainStation st : trainStations) {
+            System.out.printf("Total passengers at %s: %d\nTotal passengers serviced: %d\nAverage station wait time: %.2f\n"
+                    + "%d trains stopped at the station, with an average capacity of: %.2f \n\n", st.getName(), st.getQueueSize(),st.getCustomerServiced(), st.getMeanServiceTime(),st.getTotalTrains(),st.getMeanTrainCapacity());
+        }
     }
 
     private void passengerListReport(ArrayList<Passenger> passengers){
-        if(passengers.isEmpty() && passengers != null) {
+        if(passengers != null && !passengers.isEmpty()) {
             for(Passenger passenger : passengers) {
                 passenger.setDepartureTime();
             }
--- src/main/java/simulation/model/TrainStation.java
+++ src/main/java/simulation/model/TrainStation.java
@@ -15,11 +15,15 @@ It has an event for departure of the train, and one for the arrival of it.
 */
 
 public class TrainStation extends ServicePoint{
+    private int totalTrainCapacity;
+    private int totalTrains;
     private int currentCapacity;
     private final ContinuousGenerator trainCapacityGenerator;
     public TrainStation(String name, ContinuousGenerator stationWaitTime, ContinuousGenerator trainCapacity, EventList tl, EventType departureType) {
         super(name,stationWaitTime,tl,departureType);
         this.trainCapacityGenerator = trainCapacity;
+        this.totalTrainCapacity = 0;
+        this.totalTrains = 0;
     }
     public ArrayList<Passenger> loadTrain(){
         ArrayList<Passenger> loadedPassengers = new ArrayList<>();
@@ -39,11 +43,19 @@ public class TrainStation extends ServicePoint{
     @Override
     public void beginService(){
         currentCapacity = (int) trainCapacityGenerator.sample();
+        this.totalTrainCapacity += currentCapacity;
+        this.totalTrains++;
         System.out.printf("%sTrain arrived at %s, capacity of %d.\nThe station currently has: %d passengers.%s\n",ServicePoint.GREEN,super.name,currentCapacity,queue.size(),ServicePoint.WHITE);
         long serviceTime = (long) super.generator.sample();
         super.reserved = true;
         super.serviceTimeSum += serviceTime;
         eventList.add(new Event(super.eventTypeScheduled, Clock.getInstance().getTime() + serviceTime));
+    }
+    public int getTotalTrains(){
+        return totalTrains;
+    }
+    public double getMeanTrainCapacity(){
+        return (double) totalTrainCapacity / totalTrains;
     }
 
 }
