d074d2e7c66a77efa5ca9d0e1455552ffb420166
Minimal-Alexi <alexpop@metropolia.fi>
2024-12-03 15:19:27 +0200

    Adjusted generator to be a *bit* more realistic.

--- src/main/java/simulation/model/TrainStation.java
+++ src/main/java/simulation/model/TrainStation.java
@@ -18,15 +18,22 @@ public class TrainStation extends ServicePoint{
     private int totalTrainCapacity;
     private int totalTrains;
     private int currentCapacity;
+    private int totalLoadedCapacity;
+    private long totalTrainTravelTime;
+    private long lastDepartureTime;
     private final ContinuousGenerator trainCapacityGenerator;
     public TrainStation(String name, ContinuousGenerator stationWaitTime, ContinuousGenerator trainCapacity, EventList tl, EventType departureType) {
         super(name,stationWaitTime,tl,departureType);
         this.trainCapacityGenerator = trainCapacity;
         this.totalTrainCapacity = 0;
         this.totalTrains = 0;
+        this.totalTrainTravelTime = 0;
+        this.lastDepartureTime = 0;
+        this.totalLoadedCapacity = 0;
     }
     public ArrayList<Passenger> loadTrain(){
         ArrayList<Passenger> loadedPassengers = new ArrayList<>();
+        lastDepartureTime = Clock.getInstance().getTime();
         while(!queue.isEmpty() && currentCapacity > 0){
             Passenger passenger = super.queue.remove();
             customerServiced++;
@@ -35,7 +42,8 @@ public class TrainStation extends ServicePoint{
         }
         reserved = false;
         if(!loadedPassengers.isEmpty()){
-            System.out.printf("%sTrain leaved from %s. The train loaded %d passengers, leaving the station with : %d passengers. %s",ServicePoint.GREEN,super.name,loadedPassengers.size(),queue.size(),ServicePoint.WHITE);
+            System.out.printf("%sTrain leaved from %s. The train loaded %d passengers, leaving the station with : %d passengers. %s\n",ServicePoint.GREEN,super.name,loadedPassengers.size(),queue.size(),ServicePoint.WHITE);
+            totalLoadedCapacity += loadedPassengers.size();
             return loadedPassengers;
         }
         return null;
@@ -47,10 +55,15 @@ public class TrainStation extends ServicePoint{
         this.totalTrainCapacity += currentCapacity;
         this.totalTrains++;
         System.out.printf("%sTrain arrived at %s, capacity of %d.\nThe station currently has: %d passengers.%s\n",ServicePoint.GREEN,super.name,currentCapacity,queue.size(),ServicePoint.WHITE);
+        long arrivalTime = Clock.getInstance().getTime();
+        addTravelTime(arrivalTime-lastDepartureTime);
         long serviceTime = (long) super.generator.sample();
         super.reserved = true;
         super.serviceTimeSum += serviceTime;
         eventList.add(new Event(super.eventTypeScheduled, Clock.getInstance().getTime() + serviceTime));
+    }
+    private void addTravelTime(long newTravelTime){
+        totalTrainTravelTime += newTravelTime;
     }
     public int getTotalTrains(){
         return totalTrains;
@@ -58,5 +71,6 @@ public class TrainStation extends ServicePoint{
     public double getMeanTrainCapacity(){
         return (double) totalTrainCapacity / totalTrains;
     }
-
+    public double getMeanTravelTime(){return (double) totalTrainTravelTime / totalTrains;}
+    public double getMeanLoadedCapacity(){return (double) totalLoadedCapacity / totalTrains;}
 }
