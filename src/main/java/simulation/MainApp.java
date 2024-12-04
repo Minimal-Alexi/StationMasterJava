f060491c7373271e68d609f5921118100a6fe3c7
locphuocdang <dangloc2407@gmail.com>
2024-12-04 11:18:37 +0200

    modify for base setup

---
+++ src/main/java/simulation/MainApp.java
@@ -0,0 +1,9 @@
+package simulation;
+
+import simulation.view.StationGUI;
+
+public class MainApp {
+    public static void main(String[] args) {
+        StationGUI.launch(StationGUI.class);
+    }
+}
--- src/main/java/simulation/view/StationGUI.java
+++ src/main/java/simulation/view/StationGUI.java
@@ -1,6 +1,5 @@
 package simulation.view;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
