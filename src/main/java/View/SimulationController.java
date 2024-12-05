53c1357cdb8441e200eca619b24c07cdab99b4aa
locphuocdang <dangloc2407@gmail.com>
2024-12-05 14:53:10 +0200

    create basic simulation screen,simulation controller but can not make the animation

---
+++ src/main/java/View/SimulationController.java
@@ -0,0 +1,56 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SimulationController {
+
+    @FXML
+    private Label speedLabel;
+
+    @FXML
+    private Label timeLabel;
+
+    @FXML
+    private Label ticketCheck1Label;
+
+    @FXML
+    private Label ticketCheck2Label;
+
+    @FXML
+    private Label metroStationLabel;
+
+    @FXML
+    private Label trainStation1Label;
+
+    @FXML
+    private Label trainStation2Label;
+
+    public void updateSpeed(double speed) {
+        speedLabel.setText("Speed: " + speed);
+    }
+
+    public void updateTime(double time) {
+        timeLabel.setText("Time: " + time);
+    }
+
+    public void updateTicketCheck1(int customersServed) {
+        ticketCheck1Label.setText(String.valueOf(customersServed));
+    }
+
+    public void updateTicketCheck2(int customersServed) {
+        ticketCheck2Label.setText(String.valueOf(customersServed));
+    }
+
+    public void updateMetroStation(int customersServed) {
+        metroStationLabel.setText(String.valueOf(customersServed));
+    }
+
+    public void updateTrainStation1(int customersServed) {
+        trainStation1Label.setText(String.valueOf(customersServed));
+    }
+
+    public void updateTrainStation2(int customersServed) {
+        trainStation2Label.setText(String.valueOf(customersServed));
+    }
+}
--- src/main/java/View/StationGUI.java
+++ src/main/java/View/StationGUI.java
@@ -2,18 +2,33 @@ package View;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class StationGUI extends Application {
 
+    private static StationGUI instance;
+    private SimulationController simulationController;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/home.fxml"));
-        Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("StationMaster Simulation");
-        primaryStage.setScene(scene);
+        instance = this;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/simulation.fxml"));
+        Parent root = loader.load();
+        simulationController = loader.getController();
+
+        primaryStage.setTitle("Simulation");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static StationGUI getInstance() {
+        return instance;
+    }
+
+    public SimulationController getSimulationController() {
+        return simulationController;
     }
 
     public static void main(String[] args) {
